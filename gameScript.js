f714555d655e594e747e709624e9a82bf5e88528
Conor Moran <{ID}+{username}@users.noreply.github.com>
2023-07-21 15:15:14 +0100

    Edits to index.html, styles.css and gameScript.js

--- gameScript.js
+++ gameScript.js
@@ -24,14 +24,17 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "scissors") {
         plr_score++
         document.getElementById("player_score").textContent = "Player Score: " + plr_score
+        document.getElementById("commentary").textContent = "Player chose Rock and Computer chose Scissors"
         return "You Win!";
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         plr_score++
         document.getElementById("player_score").textContent = "Player Score: " + plr_score
+        document.getElementById("commentary").textContent = "Player chose Paper and Computer chose Rock"
         return "You Win!";
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         plr_score++
         document.getElementById("player_score").textContent = "Player Score: " + plr_score
+        document.getElementById("commentary").textContent = "Player chose Scissors and Computer chose Paper"
         return "You Win!";
     }
 
@@ -39,6 +42,7 @@ function playRound(playerSelection, computerSelection) {
     if (computerSelection == "rock" && playerSelection == "scissors") {
         comp_score++
         document.getElementById("computer_score").textContent = "Computer Score: " + comp_score
+        document.getElementById("commentary").textContent = "Player chose Rock and Computer chose Scissors"
         return "Computer Wins!";
     } else if (computerSelection == "paper" && playerSelection == "rock") {
         comp_score++
@@ -49,7 +53,6 @@ function playRound(playerSelection, computerSelection) {
         document.getElementById("computer_score").textContent = "Computer Score: " + comp_score
         return "Computer Wins!";
     }
-
     // Draw Logic
     if (playerSelection == computerSelection) {
         return "Draw!";
@@ -57,52 +60,49 @@ function playRound(playerSelection, computerSelection) {
 }
 
 window.addEventListener("DOMContentLoaded", (event) => {
-    const el1 = document.getElementById('rock_button');
-    const el2 = document.getElementById('paper_button');
-    const el3 = document.getElementById('scissors_button');
+    const rock_button = document.getElementById('rock_button');
+    const paper_button = document.getElementById('paper_button');
+    const scissors_button = document.getElementById('scissors_button');
    
-    const el4 = document.getElementById('reset_button');
+    const reset_button = document.getElementById('reset_button');
 
-    el4.hidden = true
+    reset_button.hidden = true
 
-    if (el1) {
-        el1.addEventListener("click", function() { 
-                if (turns_left != 0) {
-                    playRound("rock", getComputerChoice())
-                }
-        })
-    }
-    
-    if (el2) {
-        el2.addEventListener("click", function() { 
-            if (turns_left != 0) {
-                playRound("paper", getComputerChoice())
-            }
+    // Choices Buttons
+
+    rock_button.addEventListener("click", function() { 
+        if (turns_left != 0) {
+            playRound("rock", getComputerChoice())
+        }
     })
-    } 
-    if (el3) {
-        el3.addEventListener("click", function() { 
-            if (turns_left != 0) {
-                playRound("scissors", getComputerChoice())
-            }
+
+    paper_button.addEventListener("click", function() { 
+        if (turns_left != 0) {
+            playRound("paper", getComputerChoice())
+        }
     })
-    }
 
-    if (el4) {
-        el4.addEventListener("click", function() { 
-            if (turns_left == 0) {
-                turns_left = 5
-                comp_score = 0
-                plr_score = 0
+    scissors_button.addEventListener("click", function() { 
+        if (turns_left != 0) {
+            playRound("scissors", getComputerChoice())
+        }
+    })
 
-                document.getElementById("turns_left").textContent = "Turns left: " + turns_left
+    // Reset Button
+
+    reset_button.addEventListener("click", function() { 
+        if (turns_left == 0) {
+            turns_left = 5
+            comp_score = 0
+            plr_score = 0
+
+            document.getElementById("turns_left").textContent = "Turns left: " + turns_left
                 
-                document.getElementById("player_score").textContent = "Player Score: " + plr_score
-                document.getElementById("computer_score").textContent = "Computer Score: " + comp_score
+            document.getElementById("player_score").textContent = "Player Score: " + plr_score
+            document.getElementById("computer_score").textContent = "Computer Score: " + comp_score
 
-                el4.hidden = true
-                el4.disabled = true;
-            }
+            reset_button.hidden = true
+            reset_button.disabled = true;
+        }
     })
-    }
 })
